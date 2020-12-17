da8e3217d7e4e80c37b0f251e5ab4e8e02a7d2ef
Ken Bannister <kb2ma@runbox.com>
2020-12-17 09:20:15 -0500

    Add /validate resource for ETag handling

--- src/etsi_plug.nim
+++ src/etsi_plug.nim
@@ -2,22 +2,28 @@
 ## to exercise a client. See https://rawgit.com/cabo/td-coap4/master/base.html
 ## for test descriptions.
 ##
+## Includes the /test, /validate resources.
+##
 ## Copyright 2020 Ken Bannister
 ##
 ## SPDX-License-Identifier: Apache-2.0
 
 import libcoap, nativesockets
-import logging, strutils
+import hashes, logging, strformat, strutils
 import conet_ctx
 
 type ValueBuffer = array[0..7, char]
   ## Holds arbitrary values for encoding/decoding with libcoap
 
+#
+# /test resource, for general GET, PUT, POST, DELETE access
+#
 var testContents = "A. Corelli"
 
 proc handleTestGet(context: CContext, resource: CResource, session: CSession,
                    req: CPdu, token: CCoapString, query: CCoapString, resp: CPdu)
                    {.exportc: "hnd_test_get", noconv.} =
+  ## TD_COAP_CORE_01, TD_COAP_CORE_05, TD_COAP_CORE_12
   if req.`type` == COAP_MESSAGE_CON:
     resp.`type` = COAP_MESSAGE_ACK
   else:
@@ -43,6 +49,7 @@ proc handleTestGet(context: CContext, resource: CResource, session: CSession,
 proc handleTestPut(context: CContext, resource: CResource, session: CSession,
                    req: CPdu, token: CCoapString, query: CCoapString, resp: CPdu)
                    {.exportc: "hnd_test_put", noconv.} =
+  ## TD_COAP_CORE_03, TD_COAP_CORE_07
   ## Expects body with some text string
   if req.`type` == COAP_MESSAGE_CON:
     resp.`type` = COAP_MESSAGE_ACK
@@ -71,6 +78,7 @@ proc handleTestPut(context: CContext, resource: CResource, session: CSession,
 proc handleTestPost(context: CContext, resource: CResource, session: CSession,
                    req: CPdu, token: CCoapString, query: CCoapString, resp: CPdu)
                    {.exportc: "hnd_test_post", noconv.} =
+  ## TD_COAP_CORE_04, TD_COAP_CORE_08
   ## Expects body with some text string
   if req.`type` == COAP_MESSAGE_CON:
     resp.`type` = COAP_MESSAGE_ACK
@@ -106,6 +114,7 @@ proc handleTestPost(context: CContext, resource: CResource, session: CSession,
 proc handleTestDelete(context: CContext, resource: CResource, session: CSession,
                    req: CPdu, token: CCoapString, query: CCoapString, resp: CPdu)
                    {.exportc: "hnd_test_delete", noconv.} =
+  ## TD_COAP_CORE_02, TD_COAP_CORE_06
   if req.`type` == COAP_MESSAGE_CON:
     resp.`type` = COAP_MESSAGE_ACK
   else:
@@ -119,6 +128,53 @@ proc handleTestDelete(context: CContext, resource: CResource, session: CSession,
   let remote = getAddrString(addr session.addr_info.remote.`addr`.sa)
   oplog.log(lvlInfo, format("DELETE /test from $#", remote))
 
+#
+# /validate resource, for ETag
+#
+# Recognized /validate resource values
+var valSourceItems = ["Now is the time", "for all good men",
+                      "to come to the aid", "of their country"]
+# Index of currently selected item for resource
+var valSourceIndex = 0
+
+proc handleValidateGet(context: CContext, resource: CResource, session: CSession,
+                       req: CPdu, token: CCoapString, query: CCoapString, resp: CPdu)
+                      {.exportc: "hnd_validate_get", noconv.} =
+  ## TD_COAP_CORE_21
+  ## Uses 4-byte hash for ETag
+  if req.`type` == COAP_MESSAGE_CON:
+    resp.`type` = COAP_MESSAGE_ACK
+  else:
+    resp.`type` = COAP_MESSAGE_NON
+  resp.code = COAP_RESPONSE_CODE_205  # Content
+
+  var chain: COptlist
+
+  # add text/plain Content-Format
+  var buf: ValueBuffer
+  let valLen = encodeVarSafe(cast[ptr uint8](buf.addr), len(buf).csize_t,
+                             0.uint)
+  var optlist = newOptlist(COAP_OPTION_CONTENT_FORMAT.uint16, valLen,
+                           cast[ptr uint8](buf.addr))
+  discard insertOptlist(addr chain, optlist)
+
+  # add ETag
+  let contents = valSourceItems[valSourceIndex]
+  let etag = hash(contents)
+  var etagBytes = newSeq[char](4)
+  for i in 0..3:
+    # define etag bytes in big endian order
+    etagBytes[3-i] = cast[char]((etag.uint and (0xFF'u shl (8*i))) shr (8*i))
+    #echo(format("etag $#: $#", $(3-i), fmt"{etagBytes[3-i].uint8:02X}"))
+  optlist = newOptlist(COAP_OPTION_ETAG.uint16, len(etagBytes).csize_t,
+                       cast[ptr uint8](etagBytes[0].addr))
+  discard insertOptlist(addr chain, optlist)
+  discard addOptlistPdu(resp, addr chain)
+
+  discard addData(resp, len(contents).csize_t, contents)
+    
+  let remote = getAddrString(addr session.addr_info.remote.`addr`.sa)
+  oplog.log(lvlInfo, format("GET /validate from $#", remote))
 
 proc initResources*(ctx: CContext) =
   var r = initResource(makeStringConst("test"), 0)
@@ -127,3 +183,7 @@ proc initResources*(ctx: CContext) =
   registerHandler(r, COAP_REQUEST_POST, handleTestPost)
   registerHandler(r, COAP_REQUEST_DELETE, handleTestDelete)
   addResource(ctx, r)
+
+  var r2 = initResource(makeStringConst("validate"), 0)
+  registerHandler(r2, COAP_REQUEST_GET, handleValidateGet)
+  addResource(ctx, r2)
